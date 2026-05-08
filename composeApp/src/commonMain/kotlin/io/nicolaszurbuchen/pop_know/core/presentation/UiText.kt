6aab08927daed3453b3906e80dc6b18262fc743b
Nicolas Zurbuchen <nicolas.zurbuchen@outlook.com>
2026-05-08 23:45:22 +0900

    Improve UiText.

--- composeApp/src/commonMain/kotlin/io/nicolaszurbuchen/pop_know/core/presentation/UiText.kt
+++ composeApp/src/commonMain/kotlin/io/nicolaszurbuchen/pop_know/core/presentation/UiText.kt
@@ -8,11 +8,11 @@ import org.jetbrains.compose.resources.stringResource
 
 sealed class UiText {
     data class Dynamic(val value: String) : UiText()
-    data class Resource(val resId: StringResource) : UiText()
+    data class Resource(val resId: StringResource, val args: List<Any> = emptyList()) : UiText()
 }
 
 @Composable
 fun UiText.asString(): String = when (this) {
     is Dynamic -> value
-    is Resource -> stringResource(resId)
+    is Resource -> stringResource(resId, *args.toTypedArray())
 }
